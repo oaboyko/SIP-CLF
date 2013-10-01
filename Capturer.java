7887cd3401bfb1f6d5726c743a39ddd288c6a6f5
Oleksandr Boyko <oaboyko@ncsu.edu>
2013-10-01 08:39:38 -0400

    1. Modified the Capturer to get the payload of a packet. 2. Removed testing.txt

--- Capturer.java
+++ Capturer.java
@@ -1,13 +1,10 @@
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 import org.jnetpcap.Pcap;
 import org.jnetpcap.PcapIf;
-import org.jnetpcap.nio.JMemory;
 import org.jnetpcap.packet.*;
-import org.jnetpcap.protocol.*;
 
 public class Capturer {
 
@@ -28,6 +25,41 @@ public class Capturer {
 			System.out.printf("#%d: %s [%s]\n", i++, device.getName(), description);
 		}
 		
+		PcapIf device = allDevs.get(0);
+		int p_length = 64*1024;
+		int flags = Pcap.MODE_PROMISCUOUS;
+		int timeout = 5*1000;
+		
+		Pcap pcap = Pcap.openLive(device.getName(), p_length, flags, timeout, errBuf);
+		
+		if (pcap == null) {  
+            System.err.printf("Error while opening device for capture: "  
+                + errBuf.toString());  
+            return;  
+        }
+		
+		PcapPacketHandler<String> jpacketHandler = new PcapPacketHandler<String>(){
+			//Payload payload = new Payload();
+			public void nextPacket(PcapPacket packet, String user){
+				/*if(packet.hasHeader(payload)){
+					//String payloadS = payload.getUTF8String(0, payload.size());
+					byte[] payloadCont = payload.getByteArray(0, payload.size());
+					StringBuilder sb = new StringBuilder();
+					for(byte b : payloadCont){
+						sb.append(String.format("%02X", b));
+					}
+					System.out.printf("--->Payload: %s\n", sb.toString());
+				}*/
+				/*Print the whole packet*/
+				System.out.printf("Received packet at %s caplen=%-4d %s\n", 
+						new Date(packet.getCaptureHeader().timestampInMillis()), 
+						packet.getCaptureHeader().caplen(),
+						packet.toString());
+			}
+		};
+		
+		pcap.loop(10, jpacketHandler, "jnet");
+		pcap.close();
 	}
 
 }
